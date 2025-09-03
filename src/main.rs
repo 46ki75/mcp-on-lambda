2ad4f7f94ab89c0b5a4c7abff6bd58c62a2a60d0
Ikuma Yamashita <me@ikuma.cloud>
2025-09-04 02:03:20 +0900

    fix: update main function to use run instead of run_with_streaming_response

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 #[tokio::main]
 async fn main() -> Result<(), lambda_http::Error> {
     lambda_http::tracing::init_default_subscriber();
-    lambda_http::run_with_streaming_response(lambda_http::service_fn(
+    lambda_http::run(lambda_http::service_fn(
         mcp_on_lambda::function_handler::function_handler,
     ))
     .await
